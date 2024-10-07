7e3332bf49ba911fcf10e094e695e6f6f53004ba
Alexander Paul Quinit <paulquinit@gmail.com>
2024-10-07 22:15:27 +0800

    Refactor area_of and volume_of functions in main.rs

--- exercise/b_functions/src/main.rs
+++ exercise/b_functions/src/main.rs
@@ -31,8 +31,7 @@ fn main() {
 pub fn area_of(width: i32, height: i32) -> i32 {
     width * height
 }
-  
+
 pub fn volume_of(width: i32, height: i32, depth: i32) -> i32 {
     width * height * depth
 }
-  
